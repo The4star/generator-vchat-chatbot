a873ac7c91ffab5e68ebb388fc67983ffa4d135f
Clinton Forster <clintonforster@gmail.com>
2019-12-30 15:40:57 +1100

    adjusted all files to be copied and what files and text inside the files needed to be renamed upon chatbot generation.

--- generators/app/index.js
+++ generators/app/index.js
@@ -17,6 +17,24 @@ module.exports = class extends Generator {
         name: 'chatbotName',
         message: 'What would you like to call your chat bot?',
         default: this.appname
+      },
+      {
+        type: 'input',
+        name: 'chatbotDescription',
+        message: 'Enter a description of your chatbot',
+        default: this.appname
+      },
+      {
+        type: 'input',
+        name: 'authorName',
+        message: 'Who is the author of this chatbot?',
+        default: 'N/A'
+      },
+      {
+        type: 'input',
+        name: 'companyName',
+        message: 'What is the company name (leave blank if none)?',
+        default: 'N/A'
       }
     ]);
 
@@ -27,41 +45,51 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    const { chatbotName } = this.answers
+    const { chatbotName, chatbotDescription, authorName, companyName } = this.answers
 
-    this.fs.copy(
-      this.templatePath('frontend'),
-      this.destinationPath('frontend')
+    this.fs.copyTpl(
+      this.templatePath(`frontend`),
+      this.destinationPath(`${ chatbotName }-frontend`),
+      {chatbotName, chatbotDescription, authorName, companyName }
     );
 
     this.fs.copyTpl(
       this.templatePath('vchat-serverless-backend'),
-      this.destinationPath(`${ chatbotName }-serverless-backend`)
+      this.destinationPath(`${ chatbotName }-serverless-backend`),
+      {chatbotName, chatbotDescription, authorName, companyName}
     );
 
     this.fs.copyTpl(
       this.templatePath('vchat-voxa'),
-      this.destinationPath(`${ chatbotName }-voxa`)
+      this.destinationPath(`${ chatbotName }-voxa`),
+      {chatbotName, chatbotDescription, authorName, companyName}
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('set-up-your-chatbot-step-by-step-guide.md'),
+      this.destinationPath(`set-up-${chatbotName}-step-by-step-guide.md`),
+      {chatbotName, chatbotDescription, authorName, companyName}
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('vchat - Utterances-en-AU.xlsx'),
+      this.destinationPath(`${chatbotName} - Utterances-en-AU.xlsx`),
+      {chatbotName, chatbotDescription, authorName, companyName}
     );
   }
+  
+  // Installs dependencies to the frontend folder 
+  // install() {
+  //   const npmdir = process.cwd() + '/frontend';
+  //   process.chdir(npmdir);
+  //   this.installDependencies({ bower: false, npm: true})
+  // }
 
-  install() {
-    const npmdir = process.cwd() + '/frontend';
-    process.chdir(npmdir);
-    this.installDependencies({ bower: false, npm: true})
-  }
-
-  installBackendDependencies() {
+  end() {
     const { chatbotName } = this.answers
-    const npmdir = process.cwd() + `/${ chatbotName }-serverless-backend`;
-    process.chdir(npmdir);
-    this.installDependencies({ bower: false, npm: true})
-  }
+    this.log(
+      yosay(`Frontend, Backend and Voxa Folders have been created, to install dependencies cd into the folders and run "npm install" or "yarn install" then follow the instructions in the "set-up-${chatbotName}-step-by-step-guide" Markdown file in the root folder to see what the end product will look like head to https://vchat-bot.netlify.com/`)
+    );
 
-  installVoxaDependencies() {
-    const { chatbotName } = this.answers
-    const npmdir = process.cwd() + `/${ chatbotName }-voxa`;
-    process.chdir(npmdir);
-    this.installDependencies({ bower: false, npm: true})
   }
 };
